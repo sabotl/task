9ac308e29c2a3e6bcef99e0f65e53f13b85cc62b
sabotl <125961033+sabotl@users.noreply.github.com>
2024-02-28 02:18:51 +0300

    Update Program.cs
    long oops

--- Program.cs
+++ Program.cs
@@ -1,57 +1,75 @@
-﻿    using System;
+using System;
+using System.Collections.Generic;
 
-    public class Solution { 
-        private static List<int> nums {  get; set; } = new List<int>();
+namespace nettask
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class Solution
+    {
+        public static List<int> nums { get; set; } = new List<int>();
+
         public Solution() { }
+
         public void AddNumberToList(int num) => nums.Add(num);
+
         public void ClearList() => nums.Clear();
-        private static int SumOfDigit(int num)
+
+        private static long SumOfDigit(int num)
         {
-            int sum = 0;
-            while(num > 0)
+            long sum = 0;
+            long n = Math.Abs((long)num);
+            while (n > 0)
             {
-                if (num != 0)
-                    sum += num % 10;
-                num /= 10;
+                sum += n % 10;
+                n /= 10;
             }
             return sum;
         }
-        public static void ShowResult()
+
+        public static int ShowResult()
         {
-            int maxSum = int.MinValue;
+            long maxSum = long.MinValue;
             int numberWithMaxSum = 0;
 
-            if(nums.Count != 0)
+            if (nums.Count != 0)
+            {
                 foreach (var num in nums)
                 {
                     var sum = SumOfDigit(num);
-                    if(maxSum < sum)
+                    if (maxSum <= sum)
                     {
                         maxSum = sum;
                         numberWithMaxSum = num;
                     }
                 }
+            }
+
             Console.WriteLine($"Число с максимальной суммой цифр: {numberWithMaxSum} = {maxSum}");
+            return numberWithMaxSum;
         }
+
     }
 
-class Program
-{
-    public static void Main(string[] args)
+
+    class Program
     {
-        Solution task = new();
-       for(; ;)
-       {
-            if(int.TryParse(Console.ReadLine(), out int number))
+        public static void Main(string[] args)
+        {
+            Solution task = new Solution();
+            for (; ; )
             {
-                if (number != 0)
-                    task.AddNumberToList(number);
-                else
+                if (int.TryParse(Console.ReadLine(), out int number))
                 {
-                    Solution.ShowResult();
-                    task.ClearList();
+                    if (number != 0)
+                        task.AddNumberToList(number);
+                    else
+                    {
+                        Solution.ShowResult();
+                        task.ClearList();
+                    }
                 }
             }
-       }
-    }
+        }    }
 }
